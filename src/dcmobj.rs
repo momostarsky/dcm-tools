0eb9b90a874471a54b0152010f19de6ae4bdd8c1
starsky <daihanzhang@gmail.com>
2025-06-05 12:00:41 +0800

    提取基本信息

--- src/dcmobj.rs
+++ src/dcmobj.rs
@@ -62,13 +62,15 @@ impl StudyInfo {
 }
 #[derive(Debug, Parser, Clone)]
 pub struct SeriesInfo {
-   pub series_num: String,
-   pub series_uid: String,
-   pub series_desc: String,
-   pub modality: String,
-   pub exam_body_part: String,
-   pub image_type: String,
-   pub accession_number: String,
+    pub series_num: String,
+    pub series_uid: String,
+    pub series_date: String,
+    pub series_time: String,
+    pub series_desc: String,
+    pub modality: String,
+    pub body_part_exam: String,
+    pub image_type: String,
+    pub accession_number: String,
 }
 impl SeriesInfo {
     // 为 PatientInfo 添加构造函数
@@ -76,43 +78,45 @@ impl SeriesInfo {
         Self {
             series_num: get_tag_value(tags::SERIES_NUMBER, obj, "".to_string()),
             series_uid: get_tag_value(tags::SERIES_INSTANCE_UID, obj, "".to_string()),
-            series_desc: get_tag_value( tags::SERIES_DESCRIPTION, obj, "".to_string()),
-            modality: "".to_string(),
-            exam_body_part: "".to_string(),
-            image_type: "".to_string(),
-            accession_number: "".to_string(),
+            series_date: get_tag_value(tags::SERIES_DATE, obj, "".to_string()),
+            series_time: get_tag_value(tags::SERIES_TIME, obj, "".to_string()),
+            series_desc: get_tag_value(tags::SERIES_DESCRIPTION, obj, "".to_string()),
+            modality: get_tag_value(tags::MODALITY, obj, "".to_string()),
+            body_part_exam: get_tag_value(tags::BODY_PART_EXAMINED, obj, "".to_string()),
+            image_type: get_tag_value(tags::IMAGE_TYPE, obj, "".to_string()),
+            accession_number: get_tag_value(tags::ACCESSION_NUMBER, obj, "".to_string()),
         }
     }
 }
 #[derive(Debug, Parser, Clone)]
 pub struct ImageInfo {
-   pub inst_num: String,
-   pub sop_uid: String,
-   pub rows: String,
-   pub cols: String,
-   pub pixel_spacing: String,
-   pub slice_location: String,
-   pub slice_thickness: String,
-   pub image_position: String,
-   pub image_orientation: String,
-   pub window_center: String,
-   pub window_width: String,
-   pub photometric_interpretation: String,
-   pub bits_allocated: String,
-   pub high_bit: String,
-   pub bit_stored: String,
-   pub samples_per_pixel: String,
-   pub pixel_representation: String,
+    pub inst_num: u16,
+    pub sop_uid: String,
+    pub rows: u16,
+    pub cols: u16,
+    pub pixel_spacing: String,
+    pub slice_location: String,
+    pub slice_thickness: String,
+    pub image_position: String,
+    pub image_orientation: String,
+    pub window_center: String,
+    pub window_width: String,
+    pub photometric_interpretation: String,
+    pub bits_allocated: u16,
+    pub high_bit: u16,
+    pub bit_stored: u16,
+    pub samples_per_pixel: u16,
+    pub pixel_representation: u16,
 }
 
 impl ImageInfo {
     // 为 PatientInfo 添加构造函数
     pub fn new(obj: &DefaultDicomObject) -> Self {
         Self {
-            inst_num: get_tag_value(tags::INSTANCE_NUMBER, obj, "".to_string()),
+            inst_num: get_tag_value(tags::INSTANCE_NUMBER, obj, 1),
             sop_uid: get_tag_value(tags::SOP_INSTANCE_UID, obj, "".to_string()),
-            rows: get_tag_value(tags::ROWS, obj, "".to_string()),
-            cols: get_tag_value(tags::COLUMNS, obj, "".to_string()),
+            rows: get_tag_value(tags::ROWS, obj, 0),
+            cols: get_tag_value(tags::COLUMNS, obj, 0),
             pixel_spacing: get_tag_value(tags::PIXEL_SPACING, obj, "".to_string()),
             slice_location: get_tag_value(tags::SLICE_LOCATION, obj, "".to_string()),
             slice_thickness: get_tag_value(tags::SLICE_THICKNESS, obj, "".to_string()),
@@ -120,12 +124,16 @@ impl ImageInfo {
             image_orientation: get_tag_value(tags::IMAGE_ORIENTATION_PATIENT, obj, "".to_string()),
             window_center: get_tag_value(tags::WINDOW_CENTER, obj, "".to_string()),
             window_width: get_tag_value(tags::WINDOW_WIDTH, obj, "".to_string()),
-            photometric_interpretation: get_tag_value(tags::PHOTOMETRIC_INTERPRETATION, obj, "".to_string()),
-            bits_allocated: get_tag_value(tags::BITS_ALLOCATED, obj, "".to_string()),
-            high_bit: get_tag_value(tags::HIGH_BIT, obj, "".to_string()),
-            bit_stored: get_tag_value(tags::BITS_STORED, obj, "".to_string()),
-            samples_per_pixel: get_tag_value(tags::SAMPLES_PER_PIXEL, obj, "".to_string()),
-            pixel_representation: get_tag_value(tags::PIXEL_REPRESENTATION, obj, "".to_string()),   
+            photometric_interpretation: get_tag_value(
+                tags::PHOTOMETRIC_INTERPRETATION,
+                obj,
+                "".to_string(),
+            ),
+            bits_allocated: get_tag_value(tags::BITS_ALLOCATED, obj, 8),
+            high_bit: get_tag_value(tags::HIGH_BIT, obj, 7),
+            bit_stored: get_tag_value(tags::BITS_STORED, obj, 7),
+            samples_per_pixel: get_tag_value(tags::SAMPLES_PER_PIXEL, obj, 1),
+            pixel_representation: get_tag_value(tags::PIXEL_REPRESENTATION, obj, 1),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 mod dcmobj;
 
+use crate::dcmobj::{ImageInfo, PatientInfo, SeriesInfo, StudyInfo};
 use clap::Parser;
 use dicom::dictionary_std::tags;
-// use dicom::object::open_file;
-// use clap::builder::TypedValueParser;
-use dicom::core::Tag;
 use dicom::object::OpenFileOptions;
 use std::path::PathBuf;
-use crate::dcmobj::{get_tag_value, PatientInfo};
 
 #[derive(Debug, Parser)]
 #[command(version)]
@@ -45,105 +42,13 @@ fn main() {
     }
     let patient_info = PatientInfo::new(&obj);
     println!("PatientInfo: {:?}", patient_info);
+    let study_info = StudyInfo::new(&obj);
+    println!("StudyInfo: {:?}", study_info);
+    let series_info = SeriesInfo::new(&obj);
+    println!("SeriesInfo: {:?}", series_info);
+    let image_info = ImageInfo::new(&obj);
+    println!("ImageInfo: {:?}", image_info);
     
-    let modality = obj.element(tags::MODALITY).unwrap();
-    let study_uid = obj.element(tags::STUDY_INSTANCE_UID).unwrap();
-    let series_uid = obj.element(tags::SERIES_INSTANCE_UID).unwrap();
-    let sop_uid = obj.element(tags::SOP_INSTANCE_UID).unwrap();
-    let study_date = obj.element(tags::STUDY_DATE).unwrap();
-    let rows = obj.element(tags::ROWS).unwrap();
-    let cols = obj.element(tags::COLUMNS).unwrap();
-    let wc = obj.element(tags::WINDOW_CENTER).unwrap();
-    let wl = obj.element(tags::WINDOW_WIDTH).unwrap();
-    let photometric_interpretation = obj.element(tags::PHOTOMETRIC_INTERPRETATION).unwrap();
-    let bits_allocated: i16 = get_tag_value(tags::BITS_ALLOCATED, &obj, 8);
-    let high_bit: i16 = get_tag_value(tags::BITS_ALLOCATED, &obj, 7);
-    let bit_stored: i16 = get_tag_value(tags::BITS_ALLOCATED, &obj, 7);
-    let samples_per_pixel: i16 = get_tag_value(tags::SAMPLES_PER_PIXEL, &obj, 1);
-    let pixel_representation: i16 = get_tag_value(tags::PIXEL_REPRESENTATION, &obj, 7);
-
-    let acc_num = obj.element(tags::ACQUISITION_NUMBER).unwrap();
-    let study_id = obj.element(tags::STUDY_ID).unwrap();
-    let study_desc = obj.element(tags::STUDY_DESCRIPTION).unwrap();
-    let series_num = obj.element(tags::SERIES_NUMBER).unwrap();
-    let series_desc = obj.element(tags::SERIES_DESCRIPTION).unwrap();
-    let image_type = obj.element(tags::IMAGE_TYPE).unwrap();
-    let inst_num = get_tag_value(tags::INSTANCE_NUMBER, &obj, 1);
-
-    let pixel_spacing = obj.element(tags::PIXEL_SPACING).unwrap();
-    let slice_location = obj.element(tags::SLICE_LOCATION).unwrap();
-    let slice_thickness = obj.element(tags::SLICE_THICKNESS).unwrap();
-    let image_position = obj.element(tags::IMAGE_POSITION_PATIENT).unwrap();
-    let image_orientation = obj.element(tags::IMAGE_ORIENTATION_PATIENT).unwrap();
-
-   
-    println!("modality:{:?}", modality.to_str());
-    println!("study_uid:{:?}", study_uid.to_str());
-    println!("series_uid:{:?}", series_uid.to_str());
-    println!("sop_uid:{:?}", sop_uid.to_str());
-    println!("study_date:{:?}", study_date.to_str());
-    let output_path = &app.output_file;
-    let json_str = format!(
-        r#"{{ 
-            "modality": "{}",
-            "study_uid": "{}",
-            "series_uid": "{}",
-            "sop_uid": "{}",
-            "study_date": "{}",
-            "rows": "{}",
-            "cols": "{}",
-            "windows_center": "{}",
-            "windows_width": "{}",
-            "instance_number:  {},
-            "study_id": "{}",
-            "study_desc": "{}",
-            "series_num": "{}",
-            "series_desc": "{}",
-            "acc_num": "{}",
-            "image_type": "{}",
-            "photometric_interpretation": "{}",
-            "bits_allocated": {},
-            "high_bit": {},
-            "bit_stored": {},
-            "samples_per_pixel": {},
-            "pixel_representation": {},
-            "pixel_spacing": "{}",
-            "slice_location": "{}",
-            "slice_thickness": "{}",
-            "image_position": "{}",
-            "image_orientation": "{}"   
-        }}"#,
-       
-        modality.to_str().unwrap(),
-        study_uid.to_str().unwrap(),
-        series_uid.to_str().unwrap(),
-        sop_uid.to_str().unwrap(),
-        study_date.to_str().unwrap(),
-        rows.to_str().unwrap(),
-        cols.to_str().unwrap(),
-        wc.to_str().unwrap(),
-        wl.to_str().unwrap(),
-        inst_num,
-        study_id.to_str().unwrap(),
-        study_desc.to_str().unwrap(),
-        series_num.to_str().unwrap(),
-        series_desc.to_str().unwrap(),
-        acc_num.to_str().unwrap(),
-        image_type.to_str().unwrap(),
-        photometric_interpretation.to_str().unwrap(),
-        bits_allocated,
-        high_bit,
-        bit_stored,
-        samples_per_pixel,
-        pixel_representation,
-        pixel_spacing.to_str().unwrap(),
-        slice_location.to_str().unwrap(),
-        slice_thickness.to_str().unwrap(),
-        image_position.to_str().unwrap(),
-        image_orientation.to_str().unwrap(),
-    );
-    assert!(std::str::from_utf8(json_str.as_bytes()).is_ok());
-    std::fs::write(output_path, json_str).expect("Unable to write file");
 }
 
 fn file_exists(p0: &PathBuf) -> bool {
